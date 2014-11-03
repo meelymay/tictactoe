143e7837b1ccece64b8716ec85ddc34c345944e5
Amelia Arbisser <amarbisser@gmail.com>
2014-11-03 13:54:21 -0800

    js comments

--- static/main.js
+++ static/main.js
@@ -1,15 +1,21 @@
 function createPlayFunc(cellTag, i, j) {
     return function(e) {
 	$.get('/play', {'i': i, 'j': j}, function(data) {
+		// do nothing if game over or space occupied
 		if ($('#winner').html() !== "" ||
 		    cellTag.html() === 'O' || cellTag.html() === 'X') {
 		    return;
 		}
+
+		// play X
 		cellTag.html('X');
+		// play O
 		if ('x' in data) {
 		    var aiPlayTag = $('#cell' + data['x'] + data['y']);
 		    aiPlayTag.html('O');
 		}
+
+		// update game status
 		var status = data['status'];
 		if (status !== null) {
 		    var message = "Well, the game is over..." + status;
@@ -30,6 +36,7 @@ $(function() {
 		var x = i;
 		var y = j;
 
+		// attach the play function to each cell
 		$(document).on('click', cellName, createPlayFunc(cellTag, i, j));
 	    }
 	}
